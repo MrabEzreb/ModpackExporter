28e62870cfae5e1b23861cc5fddf58595b9cfa32
MrabEzreb <de013b3f@opayq.com>
2015-01-22 15:30:02 -0500

    WHOO i think its done

--- src/Ezreb/Main.java
+++ src/Ezreb/Main.java
@@ -2,6 +2,7 @@ package Ezreb;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 
 
@@ -10,8 +11,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
-			modsFolder.toPath().copyTo(modsgit.toPath(), StandardCopyOption.REPLACE_EXISTING);
-			configFolder.toPath().copyTo(configgit.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			Files.copy(modsFolder.toPath(), modsgit.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			Files.copy(configFolder.toPath(), configgit.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			Files.copy(flan.toPath(), flangit.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			Files.copy(npcs.toPath(), npcsgit.toPath(), StandardCopyOption.REPLACE_EXISTING);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -20,8 +23,12 @@ public class Main {
 	public static File dotminecraft = new File(System.getProperty("user.home")+"\\AppData\\Roaming\\.minecraft\\forge");
 	public static File modsFolder = new File(dotminecraft, "mods");
 	public static File configFolder = new File(dotminecraft, "config");
+	public static File flan = new File(dotminecraft, "Flan");
+	public static File npcs = new File(dotminecraft, "customnpcs");
 	public static File gitminecraft = new File(System.getProperty("user.home")+"\\Documents\\Github\\EzrebPack");
 	public static File modsgit = new File(gitminecraft, "mods");
 	public static File configgit = new File(gitminecraft, "config");
+	public static File flangit = new File(gitminecraft, "Flan");
+	public static File npcsgit = new File(gitminecraft, "customnpcs");
 
 }
